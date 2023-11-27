9c7ec8fa84f4b4572636bc1668153d81ebc0cdb4
JulienLach <leadedchamps@gmail.com>
2023-11-27 14:16:21 +0100

    OK exercice JS

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 document.getElementById('submitButton').addEventListener('click', function () {
     var emailInput = document.getElementById('email');
+    var emailValue = emailInput.value.trim();
+    var emailErrorMessage = document.getElementById('email-error-message');
+    var emailSuccessMessage = document.getElementById('email-success-message');
 
-    if (!emailInput.value) {
-        emailInput.classList.add('error'); // Add the 'error' class to change the border color to red
-        alert('Please enter your email address.'); // You can customize the error handling, such as displaying a message
-    } else {
-        // Continue with form submission or other actions
-        alert('Form submitted successfully!');
+
+    // Regular expression to validate email format
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailValue || !emailRegex.test(emailValue)) {
+        emailInput.classList.add('error');
+        alert('Please enter a valid email address.');
+        emailErrorMessage.innerText = 'Please enter a valid email address.';
+        emailSuccessMessage.innerText = '';
+
     }
+
+    else {
+        emailInput.classList.remove('error');
+        alert('Form submitted successfully!');
+        emailErrorMessage.innerText = ''; // Clear error message
+        emailSuccessMessage.innerText = 'Thank you! An email has been send to ' + emailValue;
+    }
+
+
+
+
+
 });
